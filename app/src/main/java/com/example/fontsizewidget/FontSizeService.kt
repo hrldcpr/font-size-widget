705204a1da058d3397aceb3676748ea1b0369b61
Harold Cooper <harold@x.st>
2022-08-28 17:58:33 -0400

    New > Service > Service

---
+++ app/src/main/java/com/example/fontsizewidget/FontSizeService.kt
@@ -0,0 +1,12 @@
+package com.example.fontsizewidget
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FontSizeService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
